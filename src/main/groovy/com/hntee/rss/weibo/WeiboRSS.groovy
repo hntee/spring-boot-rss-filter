3e680aab31e3b62c04ebb79dea501f838c49b7ef
hntee <ihntee@gmail.com>
2016-09-05 13:01:10 +0800

    直接访问被403,现在改成httpbuilder

--- src/main/groovy/com/hntee/rss/weibo/WeiboRSS.groovy
+++ src/main/groovy/com/hntee/rss/weibo/WeiboRSS.groovy
@@ -1,8 +1,15 @@
 package com.hntee.rss.weibo
 
 import com.hntee.rss.RSS
+import groovy.json.JsonParserType
+import groovy.json.JsonSlurper
+import groovyx.net.http.HTTPBuilder
 import org.xml.sax.SAXParseException
+
+import java.rmi.RemoteException
+
 import static groovyx.gpars.GParsPool.withPool
+import static groovyx.net.http.ContentType.TEXT
 
 /**
  * Created by htan on 2016/8/12.
@@ -25,14 +32,21 @@ class WeiboRSS implements RSS{
     }
 
     def WeiboRSS(String userId) {
-        this.url = "http://rss.weibodangan.com/weibo/rss/${userId}/"
-        def rssText = url.toURL().getText('utf-8')
-        try {
-            rss = new XmlSlurper().parseText(rssText)
-        } catch (SAXParseException e) {
-            System.err.println rssText
-            throw new Error("解析错误，因为远程服务器返回的数据有误\nURL: ${url}")
+        def http = new HTTPBuilder('http://rss.weibodangan.com')
+        def path = "/weibo/rss/${userId}/"
+        http.get(path: path,
+                contentType: TEXT,
+                headers: ['User-Agent': "Apache HTTPClient"],
+                ){ response, stream ->
+            def rssText = stream.text
+            try {
+                rss = new XmlSlurper().parseText(rssText)
+            } catch (MissingPropertyException e) {
+                System.err.println rssText
+                throw new Error("解析错误，因为远程服务器返回的数据有误\nURL: ${url}")
+            }
         }
+
         // 获取每一条正文的全文
         withPool(10) {
             items.collectParallel {
